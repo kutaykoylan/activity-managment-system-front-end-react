2e43390aecc63241459f8799542d86d9eb3e0578
Kutay <kerimkutay@gmail.com>
2020-07-30 00:30:02 +0300

    Bar chart is good to go

--- src/pages/Activity/components/BarChartOfActivitiesModal/BarChartActivitiesModal.jsx
+++ src/pages/Activity/components/BarChartOfActivitiesModal/BarChartActivitiesModal.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-import {Modal,Button} from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import { Bar } from 'react-chartjs-2';
 
 function BarChartActivitiesModal(props) {
-    const data = [65, 59, 80, 81, 56, 55, 40];
+    const getRegistrationNumberOfActivities=async ()=>{
+        
+    }
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            fill: true,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+          }
+        ]
+      };
     return (
         <div>
             <Modal size="lg" show={props.show} onHide={props.handleClose}>
@@ -11,7 +38,7 @@ function BarChartActivitiesModal(props) {
                     <Modal.Title>Bar Chart of Activities</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                <Bar data={data} />
+                    <Bar data={data} />
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="danger" onClick={props.handleClose} >Close</Button>
